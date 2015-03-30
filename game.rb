1032ff44eb9ed91e44f41e3c473f4c8e8cf4b3bd
Robin&Lily <robin&lily@appacademy.io>
2015-03-30 17:21:29 -0400

    refactor game.rb

--- game.rb
+++ game.rb
@@ -6,27 +6,45 @@ class Game
   attr_accessor :board
 
   def initialize(num_bombs, board_size)
-    @board = Board.new(num_bombs, board_size)
   end
 
   def play
+    puts "Pick a board dimension (integer):"
+    size = gets.chomp.to_i
+
+    @board = Board.new(num_bombs(size), size)
+
     until board.win? || board.lose?
       board.display
-
-      puts "Choose a row (0-8):"
-      row = gets.chomp.to_i
-      puts "Choose a column (0-8):"
-      col = gets.chomp.to_i
-
-      puts "Choose an action: F for flag, R for reveal:"
-      action = gets.chomp.downcase
-
-      tile = board[[row, col]]
-      action == "f" ? tile.flag : tile.reveal
+      process_move(get_move)
     end
 
     message = board.win? ? "congrats!" : "sorry!"
     puts message
+  end
+
+  def num_bombs(size)
+    size / 3
+  end
+
+  def get_move
+    puts "Choose a row:"
+    row = gets.chomp.to_i
+    puts "Choose a column:"
+    col = gets.chomp.to_i
+
+    puts "Choose an action: F for flag, R for reveal:"
+    action = gets.chomp.downcase
+
+    [row, col, action]
+  end
+
+  def process_move(move)
+    row, col, action = move
+
+    tile = board[[row, col]]
+    action == "f" ? tile.flag : tile.reveal
+
   end
 
 end
